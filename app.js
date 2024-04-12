11fa9c15980e69c75e5f6fb4307b5715429c5a00
Nyuhei <nyuhei06@gmail.com>
2024-04-12 22:18:21 +0900

    スタート画面

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-// キャンバスを取得
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-var count = document.getElementById("myCount");
+isStart = true;
+// html要素の取得
+const START_BUTTON_CLICK_EVENT=document.getElementById("startButton");
+var bg = document.getElementById("bg").style;
+var startButton_visible = document.getElementById("startButton").style;
+// css
+var startScreenStyle = {
+    'background-image': 'url("img/IMG_1130.png")',
+}
+var startButtonStyle = {
+    'visibility':'visible'
+}
+// ボタンの設定
+START_BUTTON_CLICK_EVENT.addEventListener("click",()=>{
+    isStart = false;
+});
 
-// const interval = setInterval(draw, 10);
+
+// 背景を適用
+function main(){
+    for (var prop in startScreenStyle){
+        if(isStart == true){
+            startScreenStyle = {
+                'background-image': 'url("img/IMG_1130.png")'
+            }
+        }
+        else{
+            startScreenStyle = {
+                'background-image': ''
+            }
+        }
+        bg[prop] = startScreenStyle[prop]
+    }
+    for(prop in startButtonStyle){
+        if(isStart == true){
+            startButtonStyle = {
+                'visibility':'visible'
+            }
+        }
+        else{
+            startButtonStyle = {
+                'visibility':'hidden'
+            }
+        }
+        startButton_visible[prop] = startButtonStyle[prop]
+    }
+}
+setInterval(main,0)
